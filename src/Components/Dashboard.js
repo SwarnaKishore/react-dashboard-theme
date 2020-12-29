391da27045211c8ef0588b0a989b8d9fdd4c975d
SwarnaKishore <swarnakishoree@gmail.com>
2020-12-28 22:08:46 -0600

    added copy right for the website

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,10 +1,28 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 class Dashboard extends React.Component {
     render() {
       return (
-        <Button variant="contained" color="primary">Dashboard Button</Button>
+        <div>
+          <Button variant="contained" color="primary">Dashboard</Button>
+          <Copyright />
+        </div>
       );
     }
 }
